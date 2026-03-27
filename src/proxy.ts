c3cbec8c86ba28e48328f5714a09e65a3a402483
fellype <fellypr>
2026-03-26 22:30:44 -0300

    feat(src/proxy.ts): Adicionado funcionalidade de autenticação

---
+++ src/proxy.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getAuthTokenFromCookies, isAuthTokenValid } from './app/utils/authToken';
+
+export function proxy(request: NextRequest) {
+  const token = getAuthTokenFromCookies();
+
+  if (!token || !isAuthTokenValid(token)) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/summary',
+};
